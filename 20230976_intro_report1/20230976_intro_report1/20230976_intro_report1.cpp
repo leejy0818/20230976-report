ea74e257168c91196585eb8edc66d71188d6bd07
leejy0818 <leejy20040818@gmail.com>
2026-03-17 11:05:42 +0900

    수정

--- 20230976_intro_report1/20230976_intro_report1/20230976_intro_report1.cpp
+++ 20230976_intro_report1/20230976_intro_report1/20230976_intro_report1.cpp
@@ -1,18 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-	int avg = 0;
+	srand(time(NULL));
+	float avg = 0;
 	int grade[10];
 
-	for (int i = 0; i < 10; i++) {
+	/*for (int i = 0; i < 10; i++) {
 		printf("%d번째 학생 점수 입력 : ", i+1);
 		scanf("%d", &grade[i]);
 		avg = avg + grade[i];
+	}*/
+
+	for(int i = 0; i < 10; i++) {
+		grade[i] = rand() % 100 + 1;
+		avg = avg + grade[i];
+		printf("%d번째 = %d, 합산 = %f\n", i + 1, grade[i], avg);
 	}
+
 	avg = avg / 10;
 
-	printf("전체 평균 점수 : %d", avg);
+	printf("전체 평균 점수 : %f", avg);
 
 	return 0;
 }
